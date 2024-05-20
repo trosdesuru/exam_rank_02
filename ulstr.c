37009c7d04880cbba903d1db5069babb7a1ae8e7
trosdesuru <eduardhernandezventos@gmail.com>
2024-05-20 17:18:42 +0200

    Upper & Lower Case String

--- ulstr.c
+++ ulstr.c
@@ -6,16 +6,19 @@ int main(int ac, char **av)
 
 	if (ac != 2)
 	{
-		write(1, "\n", 1);
-		return 1;
+		return (write(1, "\n", 1));
 	}
 	str = av[1];
 	while (ac == 2 && *str != '\0')
 	{
 		if (*str >= 'a' && *str <= 'z')
+		{
 			*str -= 32;
+		}
 		else if (*str >= 'A' && *str <= 'Z')
+		{
 			*str += 32;
+		}
 		write(1, str, 1);
 		str++;
 	}
